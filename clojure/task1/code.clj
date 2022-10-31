04f9cc63ad775c67c244cfbe0f13051c5747ccc4
EresK <e.kuular@g.nsu.ru>
2022-10-31 11:50:18 +0700

    no equal subsequent

--- clojure/task1/code.clj
+++ clojure/task1/code.clj
@@ -1,26 +1,28 @@
 (ns task1.code)
 
-(defn cat-seq-alpha [sq alpha]
+(defn get-last-char [s]
+  (cond
+    (= (count s) 0) ""
+    :default (subs s (- (count s) 1))))
+
+(defn cat-seq-alpha' [sq ch alpha]
   (->> alpha
-       (filter (fn [letter] (false? (.contains sq letter))))
+       (filter (fn [letter] (not= ch letter)))
        (map (fn [letter] (str sq letter)))))
+
+(defn cat-seq-alpha [sq alpha]
+  (cat-seq-alpha' sq (get-last-char sq) alpha))
 
 (defn cat-apply [sq alpha]
   (reduce concat (map (fn [s] (cat-seq-alpha s alpha)) sq)))
 
-(defn cat-n' [n sq alpha]
-  (if (<= n 1)
-    sq
-    (cat-n' (dec n) (cat-apply sq alpha) alpha)))
-
 (defn cat-n [n alpha]
-  (if (<= n 1)
-    alpha
-    (cat-n' n alpha alpha)))
+  (cond
+    (<= n 1) alpha
+    (> n (count alpha)) '()
+    :default  (reduce (fn [sq _] (cat-apply sq alpha)) alpha (range (dec n)))))
 
 (def alphabet '("a" "b" "c" "d"))
 
-(println (cat-apply alphabet alphabet))
-(println (cat-apply (cat-apply alphabet alphabet) alphabet))
-(println (cat-apply (cat-apply (cat-apply alphabet alphabet) alphabet) alphabet))
-(println (cat-n 1 alphabet))
+(println (cat-n 3 alphabet))
+(println (count (cat-n 3 alphabet)))
