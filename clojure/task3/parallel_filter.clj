3d6805ea93892deeb30368c8128cbb489519d9b5
EresK <e.kuular@g.nsu.ru>
2022-12-05 19:17:04 +0700

    add task 3

---
+++ clojure/task3/parallel_filter.clj
@@ -0,0 +1,32 @@
+(ns task3.parallel-filter)
+
+(defn heavy-odd? [n]
+  (Thread/sleep 1)
+  (odd? n))
+
+(defn p-filter-1 [pred coll]
+  (let [chunk-size (int (Math/ceil (Math/sqrt (count coll)))),
+        parts (partition-all chunk-size coll)]
+    (->> (pmap (partial (fn [p] (filter pred p))) parts)
+         (reduce concat))))
+
+(defn p-filter-2 [pred coll]
+  (let [chunk-size (int (Math/ceil (Math/sqrt (count coll)))),
+        parts (partition-all chunk-size coll)]
+    (->> parts
+         (map (fn [p] (future (filter pred p))))
+         (doall)
+         (map deref)
+         (reduce concat))))
+
+(def upper-bound 1000)
+
+(println "odd?")
+(time (nth (filter odd? (range 1 upper-bound)) (- (/ upper-bound 2) 1)))
+(time (p-filter-1 odd? (range 1 upper-bound)))
+(time (p-filter-2 odd? (range 1 upper-bound)))
+
+(println "heavy-odd?")
+(time (nth (filter heavy-odd? (range 1 upper-bound)) (- (/ upper-bound 2) 1)))
+(time (p-filter-1 heavy-odd? (range 1 upper-bound)))
+(time (p-filter-2 heavy-odd? (range 1 upper-bound)))
