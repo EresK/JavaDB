d551c0f0c1a939a90f502ebe75350aef80acdf03
EresK <e.kuular@g.nsu.ru>
2023-05-24 15:39:16 +0700

    query for total amount

--- database/d4/d4.sql
+++ database/d4/d4.sql
@@ -1,21 +1,18 @@
-select r.flight_no, r.aircraft_code, s.seat_no, s.fare_conditions
-from routes as r
-join seats as s on s.aircraft_code=r.aircraft_code;
+create table routes_with_seats as (
+	select r.flight_no, s.seat_no, s.fare_conditions
+	from routes as r
+	join seats as s on s.aircraft_code=r.aircraft_code
+);
 
-
-select tf.flight_id, tf.fare_conditions, tf.amount, bp.seat_no
-from ticket_flights as tf
-join boarding_passes as bp on bp.flight_id=tf.flight_id and bp.ticket_no=tf.ticket_no;
-
-
-select f.flight_no, swa.fare_conditions, swa.seat_no, min(swa.amount)::numeric(10,2) as amount
-from (select tf.flight_id, tf.fare_conditions, tf.amount, bp.seat_no
-		from ticket_flights as tf
-		join boarding_passes as bp on bp.flight_id=tf.flight_id and bp.ticket_no=tf.ticket_no
-	 ) as swa
-join flights as f on f.flight_id=swa.flight_id
-group by f.flight_no, swa.fare_conditions, swa.seat_no;
-
+create table routes_seats_amount as (
+	select f.flight_no, swa.fare_conditions, swa.seat_no, avg(swa.amount)::numeric(10,2) as amount
+	from (select tf.flight_id, tf.fare_conditions, tf.amount, bp.seat_no
+			from ticket_flights as tf
+			join boarding_passes as bp on bp.flight_id=tf.flight_id and bp.ticket_no=tf.ticket_no
+		 ) as swa
+	join flights as f on f.flight_id=swa.flight_id
+	group by f.flight_no, swa.fare_conditions, swa.seat_no
+);
 
 create table partial_amount as (
 	select rws.flight_no, rws.seat_no, rws.fare_conditions,
@@ -24,21 +21,10 @@ create table partial_amount as (
 		 	then substring(rws.seat_no from 1 for 2)
 		 	else substring(rws.seat_no from 1 for 1)
 		 end) as row_no
-	from (select r.flight_no, r.aircraft_code, s.seat_no, s.fare_conditions
-			from routes as r
-			join seats as s on s.aircraft_code=r.aircraft_code
-		 ) as rws	
-	left join (select f.flight_no, swa.fare_conditions, swa.seat_no, avg(swa.amount)::numeric(10,2) as amount
-				from (select tf.flight_id, tf.fare_conditions, tf.amount, bp.seat_no
-						from ticket_flights as tf
-						join boarding_passes as bp on bp.flight_id=tf.flight_id and bp.ticket_no=tf.ticket_no
-					 ) as swa
-				join flights as f on f.flight_id=swa.flight_id
-				group by f.flight_no, swa.fare_conditions, swa.seat_no
-			  ) as rsa
+	from routes_with_seats as rws
+	left join routes_seats_amount as rsa
 		on rsa.flight_no=rws.flight_no and rsa.seat_no=rws.seat_no and rsa.fare_conditions=rws.fare_conditions
 );
-
 
 create table partial_zero as (
 	select *
@@ -46,21 +32,18 @@ create table partial_zero as (
 	where amount = 0
 );
 
-
 create table partial_nonzero as (
 	select *
 	from partial_amount
 	where amount != 0
 );
 
-
 create table restored_amount as (
-	select pz.flight_no, pz.fare_conditions, pz.seat_no, pz.row_no, min(pn.amount)::numeric(10,2) as amount
+	select pz.flight_no, pz.fare_conditions, pz.seat_no, pz.row_no, max(pn.amount)::numeric(10,2) as amount
 	from partial_zero as pz
 	inner join partial_nonzero as pn on pn.flight_no=pz.flight_no and pn.fare_conditions=pz.fare_conditions and pn.row_no=pz.row_no
 	group by pz.flight_no, pz.seat_no, pz.fare_conditions, pz.row_no
 );
-
 
 create table full_amount as (
 	select pa.flight_no, pa.fare_conditions, pa.seat_no,
@@ -75,5 +58,18 @@ create table full_amount as (
 	left join restored_amount as ra on ra.flight_no=pa.flight_no and ra.fare_conditions=pa.fare_conditions and ra.seat_no=pa.seat_no
 );
 
+create table total_amount as (
+	select f.flight_no, (case when f.amount != fg.amount then f.fare_conditions || '+' else f.fare_conditions end) as fare_conditions,
+		f.seat_no, f.amount
+	from full_amount as f
+	join (select flight_no, fare_conditions, min(amount) as amount
+			from full_amount
+			group by flight_no, fare_conditions
+		 ) as fg
+		on fg.flight_no=f.flight_no and fg.fare_conditions=f.fare_conditions
+	order by f.flight_no desc, f.seat_no asc
+);
 
-drop table partial_amount, partial_nonzero, partial_zero, restored_amount;
+drop table routes_with_seats, routes_seats_amount, partial_amount, partial_zero, partial_nonzero, restored_amount, full_amount;
+
+select * from total_amount;
