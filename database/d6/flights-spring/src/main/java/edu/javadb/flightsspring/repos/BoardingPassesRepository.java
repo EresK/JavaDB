6944ca62eb64107692e59c5d704f75c0c0c98f10
EresK <e.kuular@g.nsu.ru>
2023-06-11 13:10:40 +0700

    add partial key search

--- database/d6/flights-spring/src/main/java/edu/javadb/flightsspring/repos/BoardingPassesRepository.java
+++ database/d6/flights-spring/src/main/java/edu/javadb/flightsspring/repos/BoardingPassesRepository.java
@@ -4,5 +4,8 @@ import edu.javadb.flightsspring.domain.BoardingPassEntity;
 import edu.javadb.flightsspring.domain.BoardingPassKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardingPassesRepository extends JpaRepository<BoardingPassEntity, BoardingPassKey> {
+    List<BoardingPassEntity> findAllByBoardingPassKeyFlightId(int flightId);
 }
--- database/d6/flights-spring/src/main/java/edu/javadb/flightsspring/repos/SeatsRepository.java
+++ database/d6/flights-spring/src/main/java/edu/javadb/flightsspring/repos/SeatsRepository.java
@@ -4,5 +4,8 @@ import edu.javadb.flightsspring.domain.SeatEntity;
 import edu.javadb.flightsspring.domain.SeatKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SeatsRepository extends JpaRepository<SeatEntity, SeatKey> {
+    List<SeatEntity> findAllBySeatKeyAircraftCode(String aircraftCode);
 }
